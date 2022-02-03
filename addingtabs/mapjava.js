ffb015a234014a6cd1ccbb314c0bc994faa3b318
s-behanh <90272717+s-behanh@users.noreply.github.com>
2022-02-03 01:23:08 -0800

    Update mapjava.js

--- addingtabs/mapjava.js
+++ addingtabs/mapjava.js
@@ -1,7 +1,15 @@
-/* Set the size of the div element that contains the map */
-#map {
-    height: 400px;
-    /* The height is 400 pixels */
-    width: 100%;
-    /* The width is the width of the web page */
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: -25.344, lng: 131.036 };
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
   }
